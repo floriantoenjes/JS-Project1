2e156a39e6f4589217fdb3ef497c44a919d378ad
Florian Tönjes <skatepainter@yahoo.de>
2017-02-20 15:20:30 +0100

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,46 @@
+"use strict;"
+
+// event listener to respond to "Show another quote" button clicks
+// when user clicks anywhere on the button, the "printQuote" function is called
+document.getElementById('loadQuote').addEventListener("click", printQuote, false);
+
+const quotes = [
+    {
+        quote: "If your hate could be turned into electricity, it would light up the whole world.",
+        source: "Nikola Tesla"
+    },
+    {
+        quote: "You can spend minutes, hours, days, weeks, or even months over-analyzing a situation; trying to put the pieces together, justifying what could've, would've happened... or you can just leave the pieces on the floor and move the fuck on.",
+        source: "Tupac Shakur"
+    },
+    {
+        quote: "In a dark place we find ourselves, and a little more knowledge lights our way.",
+        source: "Yoda",
+        citation: "Star Wars: Episode III",
+        year: 2005
+    },
+    {
+        quote: "I’m not in this world to live up to your expectations and you’re not in this world to live up to mine.",
+        source: "Bruce Lee"
+    }
+]
+
+function getRandomQuote() {
+    let randomIndex = Math.floor(Math.random() * quotes.length);
+    console.log(`Index: ${randomIndex}`);
+    return quotes[randomIndex];
+}
+
+function printQuote() {
+    let randomQuote = getRandomQuote();
+    let html = `<p class="quote">${randomQuote.quote}</p>`;
+        html += `<p class="source">${randomQuote.source}`;
+        if (randomQuote.citation) {
+            html += `<span class="citation">${randomQuote.citation}</span>`;
+        }
+        if (randomQuote.year)
+            html += `<span class="year">${randomQuote.year}</span>`;
+        html += "</p>";
+
+    document.getElementById("quote-box").innerHTML = html;
+}
